4e487c3b0d8154ae78c44656c0e569e00928fee5
Greg Elin Admin <gregelinadmin@Gregs-MacBook-Pro-M1.local>
2021-12-18 19:42:54 -0600

    [WIP] Better loop for comparing components

--- controls/views.py
+++ controls/views.py
@@ -460,6 +460,37 @@ def compare_components(request):
 
     checks = json.loads(request.POST.get('hiddenChecks'))
     compare_list = list(checks.values())
+    # Get prototype smts for all components sorted by class(catalog), sid, pid, cmpt
+    smts_all = Statement.objects.filter(producer_element__id__in=compare_list,
+                                        statement_type=StatementTypeEnum.CONTROL_IMPLEMENTATION_PROTOTYPE.name
+                                        ).order_by('sid_class', 'sid', 'pid', 'producer_element__id')
+    print(smts_all)
+    # Iterate through controls pushing all the same catalog, sid, pid onto a stack
+    cur_key = None
+    smt_diffs = {}
+    for smt in smts_all:
+        print(f"smt.id {smt.id}")
+        smt_key = f"{smt.sid_class}+{smt.sid}+{smt.pid}"
+        print(smt_key)
+        if smt_key != cur_key:
+            cur_key = smt_key
+            smt_diffs[cur_key] = {}
+            smt_diffs[cur_key][smt.producer_element.id] = {"smt_id": smt.id, "diff": "n/a"}
+        else:
+            smt_diffs[cur_key][smt.producer_element.id] = {"smt_id": smt.id, "diff": "n/a"}
+    print("=======")
+    print(smt_diffs)
+    # class-pid-sid = {cmpt1:{}, cmpt2:{}, cmpt3:{}}
+    # {'NIST_SP-800-53_rev4+at-2+at-2': {1: {'smt_id': 1, 'diff': 'n/a'}, 2: {'smt_id': 6, 'diff': 'n/a'}}, 
+    #  'NIST_SP-800-53_rev4+at-2.2+2':  {1: {'smt_id': 3, 'diff': 'n/a'}, 2: {'smt_id': 7, 'diff': 'n/a'}},
+    #  'NIST_SP-800-53_rev4+at-3+at-3': {1: {'smt_id': 4, 'diff': 'n/a'}, 2: {'smt_id': 8, 'diff': 'n/a'}}}
+    # Turn or dictionary into and ordered array
+    # Loop through our list and update the diffs and pad any missing information
+    # Pad missing information so every component has information
+    # Finish with a simple array of comparisons to display
+
+
+    # Old code below
     if len(compare_list) <= 1:
         # add messages
         messages.add_message(request, messages.WARNING, f"Not enough components were selected to compare!")
@@ -494,7 +525,8 @@ def compare_components(request):
             "prime_smts": compare_prime_smts,
             "secondary_smts": cmt_smts,
             "differences": difference_tuples,
-            "compare_list": compare_list
+            "compare_list": compare_list,
+            "smt_diffs": smt_diffs,
         }
         return render(request, "components/compare_components.html", context)
 
