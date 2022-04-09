9f7d49204a032a645c21ac247abce914df03dc42
Greg Elin <gregelin@govready.com>
2022-04-08 22:51:28 -0500

    testing logging and authentication

--- siteapp/authentication/OIDCAuthentication.py
+++ siteapp/authentication/OIDCAuthentication.py
@@ -91,7 +91,7 @@ class OIDCAuth(OIDCAuthenticationBackend):
         access_token = token_info.get('access_token')
         # DEBUG
         LOGGER.warning(f"(1) type id_token, {type(token_payload.get('id_token', None))}, {token_payload.get('id_token', None)}")
-        LOGGER.warning(f"(1) type access_token, {access_token}, {access_token}")
+        LOGGER.warning(f"(1) type access_token, {type(access_token)}, {access_token}")
 
         # Validate the token
         payload = self.verify_token(id_token, nonce=nonce)
@@ -99,9 +99,13 @@ class OIDCAuth(OIDCAuthenticationBackend):
         LOGGER.warning(f"(1) type payload, {type(payload)}, {payload}")
 
         if payload:
-            if 'SessionToken' in payload:
-                access_token = payload.get('SessionToken', None)# DEBUG
-                LOGGER.warning(f"(2) type access_token , {type(access_token)}, {access_token}")
+            if type(payload) == type(dict())
+            import json
+            payload = json.dumps(payload)
+            LOGGER.warning(f"(2) type payload , {type(payload)}, {payload}")
+            # if 'SessionToken' in payload:
+            #     access_token = payload.get('SessionToken', None)# DEBUG
+            #     LOGGER.warning(f"(2) type access_token , {type(access_token)}, {access_token}")
 
             self.store_tokens(access_token, id_token)
             try:
